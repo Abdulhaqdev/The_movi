a12f634cbb1404281058ec6d38760473a1ff1984
Abdulhaq <nadirovabdulhaq@gamil.com>
2024-01-18 21:02:01 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Series from "./pages/Series";
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { APIKEY } from './context/context'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { APIKEY } from "./context/context";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <APIKEY.Provider value='c52f4802b8f38618a7fb179f65dfcb53'>
-        <App />
-    </APIKEY.Provider>
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <APIKEY.Provider value="c52f4802b8f38618a7fb179f65dfcb53">
+    <App />
+  </APIKEY.Provider>
+);
